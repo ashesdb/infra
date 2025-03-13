a71c1516786db3d30a6f62a3c7093eae8b13ecec
Chiel Kunkels <chiel@kunkels.me>
2025-03-13 20:53:42 +0100

    feat: add data repository

--- repositories.tf
+++ repositories.tf
@@ -15,6 +15,23 @@ resource "github_repository" "ashesdb" {
   has_wiki        = false
 }
 
+resource "github_repository" "data" {
+  name       = "data"
+  visibility = "public"
+
+  allow_merge_commit = true
+  allow_rebase_merge = false
+  allow_squash_merge = false
+
+  delete_branch_on_merge = true
+
+  has_discussions = false
+  has_downloads   = false
+  has_issues      = false
+  has_projects    = false
+  has_wiki        = false
+}
+
 resource "github_repository" "infra" {
   name       = "infra"
   visibility = "public"
